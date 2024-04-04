edfc396db1606f0299e6ea01bea309da68088cc7
Reigne <monteronaelija@yahoo.com>
2024-04-04 21:12:44 +0800

    change live url

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -4,8 +4,8 @@ let baseURL = "";
 
 {
   Platform.OS == "android"
-    ? (baseURL = "http://192.168.100.93:4000/api/v1/")
-    : (baseURL = "http://localhost:4000/api/v1/");
+    ? (baseURL = "https://teampoor-api.onrender.com/api/v1/")
+    : (baseURL = "https://teampoor-api.onrender.com/api/v1/");
 }
 
 export default baseURL;
