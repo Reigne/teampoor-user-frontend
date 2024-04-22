f4474d9b8fb17c44e0fc356cd802388de2dcf5d1
Reigne <monteronaelija@yahoo.com>
2024-04-23 04:24:52 +0800

    new backend api

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -4,10 +4,10 @@ let baseURL = "";
 
 {
   Platform.OS == "android"
-    ? (baseURL = "http://192.168.100.93:4000/api/v1/")
-    : (baseURL = "http://localhost:4000/api/v1/");
-  // ? (baseURL = "https://teampoor-api.onrender.com/api/v1/")
-  // : (baseURL = "https://teampoor-api.onrender.com/api/v1/");
+    // ? (baseURL = "http://192.168.100.93:4000/api/v1/")
+    // : (baseURL = "http://localhost:4000/api/v1/");
+  ? (baseURL = "https://teampoor-api-kh6x.onrender.com/api/v1/")
+  : (baseURL = "https://teampoor-api-kh6x.onrender.com/api/v1/");
 }
 
 export default baseURL;
